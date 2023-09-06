88f00eafa71329437229b2bc6635b7a9c0c764c3
HOME <HOME@PAPA>
2023-09-06 09:03:45 +0530

    First commit

--- src/test/java/tests/JenkinsPrameterizationTest.java
+++ src/test/java/tests/JenkinsPrameterizationTest.java
@@ -1,5 +1,34 @@
 package tests;
 
-public class JenkinsPrameterizationTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
+public class JenkinsPrameterizationTest 
+{
+  @Test
+  public void  demo() throws InterruptedException
+  {
+	  WebDriver driver=null;
+	  String browser= System.getProperty("browser");
+	  switch(browser)
+	   {
+	   case "chrome":
+		   System.setProperty("webdriver.chrome.driver","./src/main/resources/chromedriver.exe" );
+		   driver=new ChromeDriver();
+		   break;
+	   case "firefox":
+		   driver=new FirefoxDriver();
+		   break;
+	   case "edge":
+		   driver=new EdgeDriver();
+		   break;
+		   default:
+			   System.out.println("Invalid browser info");
+	   }
+	  Thread.sleep(3000);
+	  driver.quit();
+  }
 }
